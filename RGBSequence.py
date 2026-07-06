bd58d1bdfea3bd45415c5804bf21768582285202
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2026-07-06 00:30:00 -0400

    Update RGBSequence.py

--- RGBSequence.py
+++ RGBSequence.py
@@ -1,16 +1,26 @@
 import numpy as np
 
-A = 3*255/np.pi
-B = np.pi/6
-D = 255/2
+A = 3*255/np.pi # (vertical) stretch altitude to 127.5, for linear ver.
+B = np.pi/6 # (horizontal) stretch period to 12
+D = 255/2 # translates midline up to 127.5
 
 
-_channel_fncts = {
-  'pure' : # linear piecewise, ~plateaus
-    lambda c,t,mlt,mx : max(0,min(255,int(np.round( (mlt*A*np.arcsin(np.cos(B*(t-4*c))**(2*mx+1))+D) )))),
-  'visible' : # sinusoidal, ~curves
-    lambda c,t,mlt,mx : max(0,min(255,int(np.round( (mlt*255/2*np.cos(B*(t-4*c))**(2*mx+1)+D)))))
+# _channel_fncts = {
+#   'line' : # linear piecewise, ~plateaus
+#     lambda c,t,mlt,xtrm,mx : max(0,min(255,int(np.round(mlt*(xtrm* A*np.arcsin( np.cos(B*(t-4*c)) **(2*mx+1))+D) )))),
+#   'sine' : # sinusoidal, ~curves
+#     lambda c,t,mlt,xtrm,mx : max(0,min(255,int(np.round(mlt*(xtrm* 255/2*       np.cos(B*(t-4*c)) **(2*mx+1)+D)))))
+# }
+
+_channel_fncts = lambda ver,c,t,mlt,xtrm,mx : max(0,min(255,int(np.round(mlt*(xtrm* __fncts[ver]( np.cos(B*(t-4*c)) **(2*mx+1)) +D) ))))
+
+__fncts = {
+  'line' : # linear piecewise, ~plateaus
+    lambda _:A*np.arcsin(_),
+  'sine' : # sinusoidal, ~curves
+    lambda _:255/2*_
 }
+
 # channel  R y G c B m (R
 #     c =  0   1   2   (3
 #   4*c =  0   4   8   (12
@@ -19,11 +29,18 @@ _channel_fncts = {
 
 
 def color_sequence(num_colors=12, start=0, end=0,
-                   exclude_end=True, reverse=False, ver='pure',
-                   alpha=255, channels=(0,1,2), chnl_mult=1, chnl_mix=0) :
+                   exclude_end=True, reverse=False, ver='line',
+                   alpha=255, channels=(0,1,2), chnl_mult=1, chnl_extreme=1, chnl_mix=0) :
+
+  # start,end : ~ROYGBIV mod12, red=0
+  # alpha : ~RGBA [0,255]
+  # channels : set peak of RGB curve, mod3
+  # chnl_mult : vertical stretch multiplier, higher highs
+  # chnl_extreme : vertical stretch multiplier, higher highs and lower lows
+  # chnl_mix : muddle colors, whole num
 
   # check format of arguments
-  assert ver in _channel_fncts.keys(), f'ver must be in {_channel_fncts.keys()}'
+  assert ver in __fncts.keys(), f'ver must be in {__fncts.keys()}'
   assert isinstance(num_colors,int) and num_colors>0, 'num_colors must be positive int'
   assert (isinstance(start,int) or isinstance(start,float)) and (isinstance(end,int) or isinstance(end,float)), 'start and end must be number in [0,12)'
   assert start>=0 and end<12, 'start and end must be in [0,12)'
@@ -34,7 +51,11 @@ def color_sequence(num_colors=12, start=0, end=0,
 
   try : len(chnl_mult)
   except : chnl_mult = [chnl_mult]*3
-  assert len(chnl_mult)==3, 'chnl_mult must be 1-or-3-tuple of numbers'
+  assert len(chnl_mult)==3, 'chnl_extreme must be 1-or-3-tuple of numbers'
+
+  try : len(chnl_extreme)
+  except : chnl_extreme = [chnl_extreme]*3
+  assert len(chnl_extreme)==3, 'chnl_extreme must be 1-or-3-tuple of numbers'
 
   try : len(chnl_mix)
   except : chnl_mix = [chnl_mix]*3
@@ -49,9 +70,9 @@ def color_sequence(num_colors=12, start=0, end=0,
       lambda _ : _*(end-start)/(n-1)+start,
       range(num_colors))
 
-  # MAP: time -> rgba, (rrr,ggg,bbb), w/ chnl_mult
+  # MAP: time -> rgba, (rrr,ggg,bbb), w/ chnl_extreme
   my_colors = map(
-      lambda t : tuple(map( _channel_fncts[ver], channels, [t]*3, chnl_mult,chnl_mix)),
+      lambda t : tuple(map( _channel_fncts, [ver]*3, channels, [t]*3, chnl_mult,chnl_extreme,chnl_mix)),
       my_colors)
   
   # MAP: rgb -> rgba, (rrr,ggg,bbb,aaa)
